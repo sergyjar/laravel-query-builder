1bf859c6287538b8efef2f8d7e5cca333dfbe9cc
sergey <sergey@mail.com>
2024-04-18 09:16:17 +0300

    Hotfix filters for snake_case filterable fields

--- src/Traits/FiltersQuery.php
+++ src/Traits/FiltersQuery.php
@@ -24,13 +24,14 @@ trait FiltersQuery
 	 */
 	private function addFilter(string $field): void
 	{
+		$requestFilter = $this->params[$field];
 		$field = Str::camel($field);
 
 		if ($this->hasFilterCallback($field)) {
-			[$this, $field]($this->params[$field]);
+			[$this, $field]($requestFilter);
 		} else {
 			try {
-				$this->query->{"where" . ucfirst($field)}($this->params[$field]);
+				$this->query->{"where" . ucfirst($field)}($requestFilter);
 			} catch (Error) {
 				throw new FilterFieldNotFoundForModelException();
 			}
